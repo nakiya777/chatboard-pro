3989f13cabc60e03652cabe709c12b774d000941
nakiya777 <akiya777@gmail.com>
2026-01-15 15:20:47 +0900

    refactor: Phase 1 - srcディレクトリ構造の整理

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,8 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': path.resolve(__dirname, './src'),
+          '~': path.resolve(__dirname, '.')
         }
       }
     };
